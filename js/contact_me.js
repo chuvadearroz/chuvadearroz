6cb79fb8aa88158ab805999e9eabd3b295147d2e
Eduardo Santos <Eduardo Santos>
2016-07-02 17:27:26 -0300

    start

---
+++ js/contact_me.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            $('#success').html("<strong>E-mail cadastrado com sucesso. </strong>");
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
